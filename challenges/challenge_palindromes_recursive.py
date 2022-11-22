06e9fa6e06e51fe8e01332d2cf7eea54a256978f
Janaina Oliveira <janainapp@gmail.com>
2022-11-22 17:39:12 -0300

    fix: flake8 errors

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,9 +5,9 @@ def is_palindrome_recursive(word, low_index, high_index):
     elif len(word) == 1:
         return True
     elif high_index > low_index:
-        return(
+        return (
             word[low_index] == word[high_index]
-            and is_palindrome_recursive(word, low_index + 1, high_index -1)
+            and is_palindrome_recursive(word, low_index + 1, high_index - 1)
         )
-    
+
     return True
