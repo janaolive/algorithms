bdde1b187634b315298d614aba37bab8bb612086
Janaina Oliveira <janainapp@gmail.com>
2022-11-22 16:52:31 -0300

    feat req 1 + 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    time_counter = 0
+    if target_time is None or isinstance(target_time, tuple):
+        return None
+    for entry, leave in permanence_period:
+        if (entry is None or leave is None) or (
+            not isinstance(entry, int) or not isinstance(leave, int)
+        ):
+            return None
+        if entry <= target_time and target_time <= leave:
+            time_counter += 1
+    return time_counter
